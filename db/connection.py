2975c17754f3aad68b366b76614a98897c4c5c41
Javialvizu <javialvizugar@gmail.com>
2026-06-16 14:03:50 -0600

    Import of connection

---
+++ db/connection.py
@@ -0,0 +1,14 @@
+import oracledb
+
+def get_connection():
+     
+    return oracledb.connect(
+        user="TAREAS",
+        password="Javialvizu77",
+        host="192.168.1.135",
+        port=1521,
+        service_name="XEPDB1"
+
+    )
+
+print(get_connection)
